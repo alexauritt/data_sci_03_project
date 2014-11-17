f8f62ade04ffefdcaad6221d9d340ffb6ae0933f
Alex Auritt <alexauritt@yahoo.com>
2014-11-17 12:24:02 -0500

    rename data frame columns with feature names

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+####### step 1 - load and merge training and test data
 setwd("./UCI\ HAR\ Dataset/test")
 test_data <- read.table("X_test.txt")
 
@@ -8,8 +9,24 @@ merged_data <- rbind(test_data, train_data)
 
 setwd("..")
 
+
+
+####### step 2 - extract on the measurements on mean and std
+
 feature_list <- read.table("features.txt")
-filteredFeaturesColumnIndices <- grepl("[Mm]ean|[Ss]td", as.character(feature_list$V2))
+completeFeatureNames <- as.character(feature_list$V2)
+filteredFeaturesColumnIndices <- grepl("[Mm]ean|[Ss]td", completeFeatureNames)
+
 
 filteredData <- merged_data[,filteredFeaturesColumnIndices]
+
+
+####### step 3 - use descriptive activity names
+#to do
+
+####### step 4 - label the data set with descriptive variable names
+filteredFeatureNames <- completeFeatureNames[filteredFeaturesColumnIndices]
+names(filteredData) <- filteredFeatureNames
+
+####### step 5 - create 2nd independant data set
 
