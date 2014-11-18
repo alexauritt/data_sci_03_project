db6f0be61ca39ee7e89cfce9f2e69ae911ef3444
Alex Auritt <alexauritt@yahoo.com>
2014-11-18 13:29:29 -0500

    descriptive activity names

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,10 @@
 ####### step 1 - load and merge training and test data
 setwd("./UCI\ HAR\ Dataset/test")
+test_activity_values <- read.table("y_test.txt")
 test_data <- read.table("X_test.txt")
 
 setwd("../train")
+train_activity_values <- read.table("y_train.txt")
 train_data <- read.table("X_train.txt")
 
 merged_data <- rbind(test_data, train_data)
@@ -21,12 +23,25 @@ filteredFeaturesColumnIndices <- grepl("[Mm]ean|[Ss]td", completeFeatureNames)
 filteredData <- merged_data[,filteredFeaturesColumnIndices]
 
 
-####### step 3 - use descriptive activity names
-#to do
+####### step 3 - use descriptive activity 
+
+activity_table <- read.table("activity_labels.txt")
+activity_labels <- as.character(activity_table$V2)
+
+# compose numeric vector of activity values for combined test and training data. (10,299 x 1 vector)
+all_activities <- append(as.numeric(test_activity_values$V1), as.numeric(train_activity_values$V1))
+
+#convert numeric vector to factor
+all_activities <- factor(all_activities, labels=activity_labels)
+
+
 
 ####### step 4 - label the data set with descriptive variable names
 filteredFeatureNames <- completeFeatureNames[filteredFeaturesColumnIndices]
 names(filteredData) <- filteredFeatureNames
+
+#add activitiy column
+filteredData$activity <- all_activities
 
 ####### step 5 - create 2nd independant data set
 
