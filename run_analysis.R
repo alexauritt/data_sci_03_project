5687668273bc356065427b3b0ef84b8896a264ba
Alex Auritt <alexauritt@yahoo.com>
2014-11-21 23:28:08 -0500

    adding subject vector

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,11 @@
 ####### step 1 - load and merge training and test data
 setwd("./UCI\ HAR\ Dataset/test")
+test_subject_ids <- read.table("subject_test.txt")
 test_activity_values <- read.table("y_test.txt")
 test_data <- read.table("X_test.txt")
 
 setwd("../train")
+train_subject_ids <- read.table("subject_train.txt")
 train_activity_values <- read.table("y_train.txt")
 train_data <- read.table("X_train.txt")
 
@@ -28,19 +30,21 @@ filteredData <- merged_data[,filteredFeaturesColumnIndices]
 activity_table <- read.table("activity_labels.txt")
 activity_labels <- as.character(activity_table$V2)
 
-# compose numeric vector of activity values for combined test and training data. (10,299 x 1 vector)
+# compose numeric vector of subject and activity values for combined test and training data. (10,299 x 1 vector)
+all_subject_ids <- append(as.numeric(test_subject_ids$V1), as.numeric(train_subject_ids$V1))
 all_activities <- append(as.numeric(test_activity_values$V1), as.numeric(train_activity_values$V1))
 
-#convert numeric vector to factor
+#convert numeric vectors to factor
+all_subject_ids <- factor(all_subject_ids)
 all_activities <- factor(all_activities, labels=activity_labels)
-
 
 
 ####### step 4 - label the data set with descriptive variable names
 filteredFeatureNames <- completeFeatureNames[filteredFeaturesColumnIndices]
 names(filteredData) <- filteredFeatureNames
 
-#add activitiy column
+#add subject id and activity columns
+filteredData$subject_ids <- all_subject_ids
 filteredData$activity <- all_activities
 
 ####### step 5 - create 2nd independant data set
